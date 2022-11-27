edb358390c4004713a6cafa8abdaedabf42205e0
Sachin <shatikankan@gmail.com>
2022-11-27 21:35:34 +0530

    added uipath linq query blog with xamls

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  const allPostsData = getSortedPostsData();
   return {
     props: {
-      allPostsData
-    }
-  }
+      allPostsData,
+    },
+  };
 }
 export default function Home({ allPostsData }) {
   return (
@@ -19,25 +19,43 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <p>Hello, I am Sachin. I am RPA developer who helps business by automating business processes and saving FTE counts that does manual work on a particular business process.</p>
-      <p>I am a full-stack development enthusiast who likes to learn new tools and technologies to keep myself updated with the lastest tech trend.</p>
-      <p> I also do open-source contributions. You can connect with me on <a href="https://twitter.com/shatikankan">Twitter</a></p>
+      <p>
+        Hello, I am Sachin. I am RPA developer who helps business by automating
+        business processes and saving FTE counts that does manual work on a
+        particular business process.
+      </p>
+      <p>
+        I am a full-stack development enthusiast who likes to learn new tools
+        and technologies to keep myself updated with the lastest tech trend.
+      </p>
+      <p>
+        Feel free to explore the xamls and enhance your knowledge in UiPath. By
+        blogging I wish to save your time and help you learn things quickly
+        which will be helpful for the next technical feasibility which is on
+        your way which you are dreading about. Don't worry I have got your back
+        :)
+      </p>
+      <p>
+        {" "}
+        I also do open-source contributions. You can connect with me on{" "}
+        <a href="https://twitter.com/shatikankan">Twitter</a>
+      </p>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>
-              <a>{title}</a>
-            </Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
       </section>
     </Layout>
-  )
+  );
 }
---
+++ posts/uipath-linq-queries.md
@@ -0,0 +1,31 @@
+---
+title: "UiPath Linq Queries"
+date: "2022-11-27"
+hero_image: "./annie-spratt-MChSQHxGZrQ-unsplash.jpg"
+hero_image_alt: "men sitting in front of their laptop computer"
+hero_image_credit_text: "Annie Spratt"
+hero_image_credit_link: "https://unsplash.com/photos/MChSQHxGZrQ"
+---
+
+- In this blog, I have documented a series of frequently asked linq queries related interview questions.
+
+- **_What is Linq query_**
+
+  - Language-Integrated Query (LINQ) is the name for a set of technologies based on the integration of query capabilities directly into the C# language. Traditionally, queries against data are expressed as simple strings without type checking at compile time or IntelliSense support. Furthermore, you have to learn a different query language for each type of data source: SQL databases, XML documents, various Web services, and so on. With LINQ, a query is a first-class language construct, just like classes, methods, events. You write queries against strongly typed collections of objects by using language keywords and familiar operators. The LINQ family of technologies provides a consistent query experience for objects (LINQ to Objects), relational databases (LINQ to SQL), and XML (LINQ to XML).
+
+- **_Benefits of Linq Queries_**
+
+  - Familiar language: Developers don't have to learn a new query language for each type of data source or data format.
+  - Less coding: It reduces the amount of code to be written as compared with a more traditional approach.
+  - Readable code: LINQ makes the code more readable so other developers can easily understand and maintain it.
+  - Standardized way of querying multiple data sources: The same LINQ syntax can be used to query multiple data sources.
+  - Compile time safety of queries: It provides type checking of objects at compile time.
+  - IntelliSense Support: LINQ provides IntelliSense for generic collections.
+  - Shaping data: You can retrieve data in different shapes.
+
+[Refer frequently asked linq queries here ](https://github.com/SachinHatikankar100/UiPathPractice)
+
+Post reference:
+
+- https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/linq/
+- https://www.tutorialsteacher.com/linq/what-is-linq
