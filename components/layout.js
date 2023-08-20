8caa3070b7e149b50dbb709f8598147f84d15bf7
unknown <shatikankan@gmail.com>
2023-08-20 15:30:45 +0530

    updated the link description

--- components/layout.js
+++ components/layout.js
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Technical blogs from Sachin Hatikankan"
         />
         <meta
           property="og:image"
